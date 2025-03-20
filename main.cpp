a935a7ca70f0d83bc127c00845b8fa22334c5c53
Tranlequan <Tranlequan04092006@gmail.com>
2025-03-20 18:16:15 +0700

    v1

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ public:
            }
     }
     void render(SDL_Renderer* renderer){
-        SDL_SetRenderDrawColor(renderer,255,255 , 0, 255);
+        SDL_SetRenderDrawColor(renderer,255,0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
     }
 
